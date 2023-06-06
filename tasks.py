5bf438e9e1fe0f30d2ff23a64f1e34da0f892774
Damien Garros <dgarros@gmail.com>
2023-06-07 00:39:06 +0200

    Add invoke

---
+++ tasks.py
@@ -0,0 +1,35 @@
+"""Replacement for Makefile."""
+import os
+import sys
+import glob
+from datetime import datetime
+
+from typing import Tuple
+
+from invoke import task, Context  # type: ignore
+
+PROJECT_NAME = "infrahub-demo-edge-develop"
+
+def git_info(context: Context) -> Tuple[str, str]:
+    """Return the name of the current branch and hash of the current commit."""
+    branch_name = context.run("git rev-parse --abbrev-ref HEAD", hide=True, pty=False)
+    hash = context.run("git rev-parse --short HEAD", hide=True, pty=False)
+    return branch_name.stdout.strip(), hash.stdout.strip()
+
+@task
+def generate_archive(context: Context):
+    branch, commit = git_info(context=context)
+
+    package_name = f"{PROJECT_NAME}-{commit[:8]}.tar.gz"
+
+    commands = [
+        f"rm -rf /tmp/{PROJECT_NAME}",
+        f"cp -a ../{PROJECT_NAME} /tmp/{PROJECT_NAME}",
+        f"git --git-dir /tmp/{PROJECT_NAME}/.git remote remove origin",
+        f"tar -C /tmp -czf {package_name} {PROJECT_NAME}",
+    ]
+
+    for command in commands:
+        result = context.run(command=command, pty=True)
+
+    print(f"Package {package_name!r} generated successfully.")
