b4609d5812f20e084db9a327f478a1196c59d457
John Partee <morganpartee@gmail.com>
2023-11-09 11:37:49 -0600

    fix transform imports?

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,4 +1,4 @@
-from avdbgp import AristaBGP
-from openconfig import OCBGPNeighbors, OCInterfaces
+from .avdbgp import AristaBGP
+from .openconfig import OCBGPNeighbors, OCInterfaces
 
 INFRAHUB_TRANSFORMS = [AristaBGP, OCInterfaces, OCBGPNeighbors]
