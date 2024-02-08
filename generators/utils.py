30eee090a737fa50465d366799693bd79a52d430
wvandeun <wim@opsmill.com>
2024-02-08 14:27:18 +0100

    add relationship (cardinality one) inheritance to generator scripts

--- generators/utils.py
+++ generators/utils.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from infrahub_sdk import InfrahubClient, InfrahubNode
-
+from infrahub_sdk.node import RelatedNode, Attribute
 
 class InheritanceException(Exception):
     pass
@@ -9,9 +9,16 @@ class InheritanceException(Exception):
 
 async def inherit_attribute_from_hierarchy(
     client: InfrahubClient, node: InfrahubNode, attribute: str
-) -> Union[int, str, bool]:
+) -> Union[int, str, bool, InfrahubNode]:
     if hasattr(node, attribute):
-        return getattr(node, attribute).value
+        attr = getattr(node, attribute)
+        if isinstance(attr, RelatedNode) and attr.schema.cardinality == "one":
+            await attr.fetch()
+            return attr.peer
+        elif isinstance(attr, RelatedNode) and attr.schema.cardinality == "many":
+            raise InheritanceException(f"Relationships of cardinality many are not supported!")
+        elif isinstance(attr, Attribute):
+            return attr.value
 
     if not hasattr(node, "parent"):
         raise InheritanceException(f"Could not resolve {attribute} for {node.typename}")
