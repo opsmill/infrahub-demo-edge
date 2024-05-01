b9e3b0c2e1aa206f91134ccd4f12a8588e163646
Benoit Kohler <bkohler@bearchitek.tailc018d.ts.net>
2024-05-01 14:36:06 +0200

    test

--- generators/upstream_interfaces.py
+++ generators/upstream_interfaces.py
@@ -3,22 +3,28 @@ from infrahub_sdk.generator import InfrahubGenerator
 
 class Generator(InfrahubGenerator):
     async def generate(self, data: dict) -> None:
-
-        upstream_interface = data["InfraInterfaceL3"]["edges"][0]["node"]
+        # Extract the first node in the 'InfraInterfaceL3' edges array
+        upstream_interface = data["data"]["InfraInterfaceL3"]["edges"][0]["node"]
 
         provider = None
         circuit_id = None
         role: str = upstream_interface["role"]["value"]
         status: str = upstream_interface["status"]["value"]
-        if upstream_interface["connected_endpoint"]:
-            connected_endpoint = upstream_interface["connected_endpoint"]["node"]
-            if connected_endpoint["circuit"]["node"]:
-                circuit = connected_endpoint["circuit"]["node"]
-                provider: str = circuit["provider"]["node"]["name"]["value"]
-                circuit_id: str = circuit["vendor_id"]["value"]
 
-        if provider:
+        # Check and extract data from the connected endpoint
+        if "connected_endpoint" in upstream_interface and "node" in upstream_interface["connected_endpoint"]:
+            connected_endpoint = upstream_interface["connected_endpoint"]["node"]
+            if "circuit" in connected_endpoint and "node" in connected_endpoint["circuit"]:
+                circuit = connected_endpoint["circuit"]["node"]
+                if "provider" in circuit and "node" in circuit["provider"]:
+                    provider = circuit["provider"]["node"]["name"]["value"]
+                if "vendor_id" in circuit:
+                    circuit_id = circuit["vendor_id"]["value"]
+
+        # Update the object description if provider and circuit_id are available
+        if provider and circuit_id:
             new_description = f"{role.upper()}: {provider.title()}-{circuit_id.upper()} ({status.lower()})"
+            # Retrieve the object based on type and ID, then update its description
             obj = await self.client.get(kind=upstream_interface["__typename"], id=upstream_interface["id"])
             obj.description = new_description
             await obj.save(allow_upsert=True)
